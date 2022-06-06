98e4897a7af56d4b17b3f7bac3f7f764e57e27fb
Mazin Ahmos AL-tarek Awad <62282787+Mazin778@users.noreply.github.com>
2022-06-06 15:35:41 +0200

    mmnnvers

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_final_project_level1/pages/login.dart';
 import 'package:flutter_final_project_level1/pages/signup.dart';
 import 'package:flutter_final_project_level1/pages/welcome.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      initialRoute: "/" ,
+    return MaterialApp(
+      initialRoute: "/",
       routes: {
-        "/" : (context) => const Welcome(),
-        "/login" : (context) => const Login(),
-        "/signup" : (context) => const Signup(),
+        "/": (context) => const Welcome(),
+        "/login": (context) => const Login(),
+        "/signup": (context) => const Signup(),
       },
     );
   }
